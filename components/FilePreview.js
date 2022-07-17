ab14c9c0b89581785ee6bda925e303cc93a59258
appnative <krishna@cognativ.com.au>
2022-07-18 08:04:26 +1000

    initial commit

---
+++ components/FilePreview.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "../assets/styles/FilePreview.module.css";
+
+const FilePreview = ({ fileData }) => {
+  return (
+    <div className={styles.fileList}>
+      <div className={styles.fileContainer}>
+        {/* loop over the fileData */}
+        {fileData.fileList.map((f) => {
+          return (
+            <>
+              <ol>
+                <li key={f.lastModified} className={styles.fileList}>
+                  {/* display the filename and type */}
+                    <div> {console.log(f)}</div>
+                    <div> {f.type}</div>
+                  <div key={f.name} className={styles.fileName}>
+                    {f.name}
+                  </div>
+                </li>
+              </ol>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FilePreview;
---
+++ constants/config.js
@@ -0,0 +1,8 @@
+export default function config(){
+    return {
+        "dev":
+            {
+                "url": "http://127.0.0.1:8000"
+            }
+        }
+}
